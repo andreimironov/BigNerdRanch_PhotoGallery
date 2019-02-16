021951dc7779aedd2c0aadfa6d5a476c04a4a70b
andrei <andrew000@list.ru>
2019-02-16 09:45:03 +0300

    add progress bar and title

--- app/src/main/java/com/andreimironov/andrei/photogallery/PhotoPageFragment.java
+++ app/src/main/java/com/andreimironov/andrei/photogallery/PhotoPageFragment.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PhotoPageFragment extends VisibleFragment {
     private static final String ARG_URI = "photo_page_url";
     private Uri mUri;
     private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     public static PhotoPageFragment newInstance(Uri uri) {
         Bundle args = new Bundle();
@@ -33,10 +38,32 @@ public class PhotoPageFragment extends VisibleFragment {
             Bundle savedInstanceState
     ) {
         View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
+
+        mProgressBar = v.findViewById(R.id.progress_bar);
+        mProgressBar.setMax(100); // WebChromeClient reports in range 0-100
+
         mWebView = v.findViewById(R.id.web_view);
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setTitle(title);
+            }
+        });
         mWebView.setWebViewClient(new WebViewClient());
         mWebView.loadUrl(mUri.toString());
+
         return v;
     }
 }
